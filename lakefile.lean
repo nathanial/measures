3a74219bfdf5b60c143be76fb6a5a3246686b215
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-21 06:29:54 -0800

    Update crucible to v0.0.8

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ package measures where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
-require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.7"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.8"
 
 @[default_target]
 lean_lib Measures where
